824cbb065196dadbb7cae0738c6859409cb1a1cd
fernandorodriguezrada <frr20122@gmail.com>
2023-07-21 13:53:25 -0400

    first commit

--- js/scroll-script.js
+++ js/scroll-script.js
@@ -1,6 +1,6 @@
 var scrollToTopBtn = document.getElementById("scroll-to-top-btn");
 
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
   if (window.pageYOffset > 20) {
     scrollToTopBtn.classList.add("show");
     scrollToTopBtn.classList.remove("hide");
@@ -10,14 +10,14 @@ window.addEventListener("scroll", function() {
   }
 });
 
-scrollToTopBtn.addEventListener("click", function() {
+scrollToTopBtn.addEventListener("click", function () {
   window.scrollTo(0, 0);
 });
 
 var reviewsLink = document.querySelector('a[href="/stars/stars.html"]');
 
-reviewsLink.addEventListener("click", function(event) {
+reviewsLink.addEventListener("click", function (event) {
   event.preventDefault();
   var ratingBox = document.querySelector(".rating-box");
-  ratingBox.scrollIntoView({behavior: "smooth"});
+  ratingBox.scrollIntoView({ behavior: "smooth" });
 });
