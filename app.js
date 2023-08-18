ed2aa2696195324f076cc5278ee6ccc499d56117
fernandorodriguezrada <frr20122@gmail.com>
2023-08-18 19:39:14 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  const filePath = path.join(__dirname, "public", "homepage", "index.html");
+  res.sendFile(filePath);
+});
+
+app.listen(5000, function () {
+  console.log("Server is running on localhost:5000");
+});
