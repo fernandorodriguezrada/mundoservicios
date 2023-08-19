6d575412d3f8d5a54725874bcd8dfb45eaf00e41
fernandorodriguezrada <frr20122@gmail.com>
2023-08-19 02:33:29 -0400

    first commit

--- src/firebase/firebaseauth.js
+++
@@ -1,6 +0,0 @@
-// import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-// import app from "../firebase/firebase.config.js";
-
-// const auth = getAuth(app);
-
-// console.log(auth);
